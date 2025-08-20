98173b814e0a29a8b4579cd78b9e3980b86b72ce
solaire-fivem <solaire-fivem@gmail.com>
2025-08-20 18:43:06 +0100

    Add updatemugshot command

--- client/commands.lua
+++ client/commands.lua
@@ -2,3 +2,7 @@ RegisterCommand('toggleminimap', function()
     Config.ShowMinimapAlways = not Config.ShowMinimapAlways
     DisplayMinimap(Config.ShowMinimapAlways)
 end, false)
+
+RegisterCommand('updatemugshot', function()
+    TakePlayerMugshot()
+end, false)
